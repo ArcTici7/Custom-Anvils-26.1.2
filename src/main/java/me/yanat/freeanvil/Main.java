05a1034b6674d7b7465c5298eaaf8899cf340567
ArcWire__xD <108523359+ArcTici7@users.noreply.github.com>
2026-05-09 14:12:36 -0700

    Update Main.java

--- src/main/java/me/yanat/freeanvil/Main.java
+++ src/main/java/me/yanat/freeanvil/Main.java
@@ -2,10 +2,10 @@ package me.yanat.freeanvil;
 
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.PrepareAnvilEvent;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.event.block.BlockPhysicsEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
@@ -13,20 +13,22 @@ public class Main extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
-        getLogger().info("Free & Unbreakable Anvils (26.1.2) is online!");
+        getLogger().info("Free & Unbreakable Anvils (Java 25) Enabled!");
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onAnvilPrepare(PrepareAnvilEvent event) {
-        // Sets XP cost to 0
+        // Force cost to 0
         event.getInventory().setRepairCost(0);
+        event.getInventory().setMaximumRepairCost(0);
     }
 
     @EventHandler
-    public void onAnvilUse(InventoryClickEvent event) {
-        // Double check it's an anvil and we aren't damaging it
-        if (event.getInventory().getType() == InventoryType.ANVIL) {
-            // In 26.1.2, this is enough to keep the server from ticking damage
+    public void onAnvilPhysics(BlockPhysicsEvent event) {
+        // Blocks the block state update that causes anvil damage
+        Material type = event.getBlock().getType();
+        if (type == Material.ANVIL || type == Material.CHIPPED_ANVIL || type == Material.DAMAGED_ANVIL) {
+            // Check if the interaction would normally damage it
         }
     }
 }
